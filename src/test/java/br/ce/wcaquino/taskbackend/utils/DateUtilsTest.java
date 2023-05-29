e6327bf058d6b0f7b0bdabae876bd02be0abde00
Renan Lopes Goncalves <renan.lopes@BAPBR-C02G80RJ0KPF.lan>
2023-05-29 15:52:18 -0300

    Retornando testes comentados

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class DateUtilsTest {
 
-//	@Test
-//	public void deveRetornarTrueParaDatasFuturas () {
-//		LocalDate date = LocalDate.of(2030, 1, 1);
-//		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-//	}
-//	
-//	@Test
-//	public void deveRetornarFalseParaDatasPassadas () {
-//		LocalDate date = LocalDate.of(2010, 1, 1);
-//		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
-//	}
-//	
-//	@Test
-//	public void deveRetornarTrueParaDataAtual () {
-//		LocalDate date = LocalDate.now();
-//		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-//	}
+	@Test
+	public void deveRetornarTrueParaDatasFuturas () {
+		LocalDate date = LocalDate.of(2030, 1, 1);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetornarFalseParaDatasPassadas () {
+		LocalDate date = LocalDate.of(2010, 1, 1);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetornarTrueParaDataAtual () {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
 }
